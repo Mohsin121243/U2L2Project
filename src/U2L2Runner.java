d3038c01cbf853ad20ca80c666e988487c9e1052
unknown <BT_4N2_27@BKTECH.LOCAL>
2022-09-23 12:32:01 -0400

    Initial commit

---
+++ src/U2L2Runner.java
@@ -0,0 +1,16 @@
+public class U2L2Runner {
+    public static void main(String[] args) {
+        Rectangle rectA  = new Rectangle(150, 200);
+        Rectangle rectB = new Rectangle(100, 100);
+        Rectangle rectC = new Rectangle(100, 50);
+        rectA.printRectangleInfo();
+        rectB.printRectangleInfo();
+        rectC.printRectangleInfo();
+        rectA.setWidth(125);
+        rectB.setWidth(125);
+        rectB.setLength(125);
+    }
+
+    }
+
+
