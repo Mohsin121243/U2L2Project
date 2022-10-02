44c60a30a17c64918a2945ee55d764451ce6b373
Mohsin121243 <mchoudhary4645@bths.edu>
2022-10-02 10:39:54 -0400

    rectangle lengths addes

--- src/U2L2Runner.java
+++ src/U2L2Runner.java
@@ -3,12 +3,24 @@ public class U2L2Runner {
         Rectangle rectA  = new Rectangle(150, 200);
         Rectangle rectB = new Rectangle(100, 100);
         Rectangle rectC = new Rectangle(100, 50);
-        rectA.printRectangleInfo();
-        rectB.printRectangleInfo();
-        rectC.printRectangleInfo();
         rectA.setWidth(125);
         rectB.setWidth(125);
         rectB.setLength(125);
+        rectB.setWidth(125);
+        rectC.setWidth(125);
+        rectA.printRectangleInfo();
+        rectB.printRectangleInfo();
+        rectC.printRectangleInfo();
+        int rectAperimeter = (2* rectA.getLength()) + (2 * rectA.getWidth());
+        int rectBperimeter = 4 * rectB.getLength();
+        int rectCperimeter = (2* rectC.getWidth()) + (2* rectC.getLength());
+        int totalPerimeter = rectAperimeter + rectBperimeter+rectCperimeter;
+        int totalArea = (rectA.calculateArea() + rectB.calculateArea() + rectC.calculateArea());
+        System.out.println("The perimeter is: " + totalPerimeter);
+        System.out.println("The area is: "+ totalArea);
+
+
+
     }
 
     }
